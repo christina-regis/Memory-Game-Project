0775079cacc99db743ce16cb62d19e32e65a2733
christina regis <Mcfreegis11!>
2016-03-23 13:59:39 -0700

    at end of game message with button to restart

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-//when box is clicked turn to backside with color
+//when box is clicked turn to backside with picture
 var counter = 0;
 var matches = 0;
 $('.box').on('click', function(){
+  //if has class matched, card has already been matched and can't be flipped again
   if ($(this).hasClass('matched')) {
     return false;
   } else {
@@ -9,7 +10,8 @@ $('.box').on('click', function(){
   counter ++;
   //console.log(counter);
   if (counter === 2) {
-    setTimeout(function() {checkBox()}, 500);
+    //unable to finish first checkbox function before next one starts
+    setTimeout(function() {checkBox();}, 500);
     }
   }
 });
@@ -27,18 +29,31 @@ var checkBox = function(){
     $('.matchesMade').text('Matches made: ' + matches );
     $('.matchesNotify').text('You made a match!');
     setTimeout(function (){
-      $('.matchesNotify').text('')}, 800);
+      $('.matchesNotify').text('');}, 800);
       if (matches === 6) {
-        window.alert('You matched all the dogs');
+        //at the end of the game if yes to play again run same code as reset button
+        var playAgain = window.confirm('You matched all the dogs! Would you like to play again?');
+        if (playAgain === true) {
+          $('.flip').removeClass('flip');
+          $('.matched').removeClass('matched');
+          var randomDivs = $('.box').get().sort(function() {
+          return Math.round(Math.random())-0.5;
+          });
+          $(randomDivs).appendTo(randomDivs[0].parentNode).show();
+          matches = 0;
+          $('.matchesMade').text('Matches made: ' + matches);
+        } else {
+            console.log('cancel');
+        }
       }
     }
-    else {
-      $(square2).removeClass('flip');
-      $(square1).removeClass('flip');
-      counter = 0;
-    }
-  };
-
+    //if squares do not match, turns cards back over
+  else {
+    $(square2).removeClass('flip');
+    $(square1).removeClass('flip');
+    counter = 0;
+  }
+};
 $('button').on('click', function(){
   $('.flip').removeClass('flip');
   $('.matched').removeClass('matched');
@@ -47,6 +62,6 @@ $('button').on('click', function(){
   });
   $(randomDivs).appendTo(randomDivs[0].parentNode).show();
   matches = 0;
-   $('.matchesMade').text('Matches made: ' + matches );
+   $('.matchesMade').text('Matches made: ' + matches);
 });
 
