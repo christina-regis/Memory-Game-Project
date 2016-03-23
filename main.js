4ecc2a73fbfd8a3f0dd8e0c620e8bcd14f90329b
christina regis <Mcfreegis11!>
2016-03-23 10:20:48 -0700

    everything works

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
 var counter = 0;
 var matches = 0;
 $('.box').on('click', function(){
+  if ($(this).hasClass('matched')) {
+    return false;
+  } else {
+
   $(this).addClass('flip');
   counter ++;
   //console.log(counter);
   if (counter === 2) {
     setTimeout(function() {checkBox()}, 500);
     }
+  }
 });
 
 //check if both turned over cards match
@@ -16,8 +21,8 @@ var checkBox = function(){
   var square1 = $flipped[0]; //shows classes of first flipped box
   var square2 = $flipped[1]; //shows classes of 2nd flipped box
   if (square1.children[1].className === square2.children[1].className) {
-    $(square1).removeClass('flip').addClass('matched').off('click');
-    $(square2).removeClass('flip').addClass('matched').off('click');
+    $(square1).removeClass('flip').addClass('matched');
+    $(square2).removeClass('flip').addClass('matched');
     counter = 0;
     matches = matches + 1;
     $('p').text('You made a match!');
